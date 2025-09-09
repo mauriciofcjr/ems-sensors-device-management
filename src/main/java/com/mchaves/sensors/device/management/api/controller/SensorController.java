acf6b05287917c17b70a0e5b0c7ee8833b85c3d6
Mauricio Chaves <mauricio.chaves.jr@gmail.com>
2025-09-08 22:21:53 -0300

    Criação do endpoint de getOneId

--- src/main/java/com/mchaves/sensors/device/management/api/controller/SensorController.java
+++ src/main/java/com/mchaves/sensors/device/management/api/controller/SensorController.java
@@ -6,9 +6,11 @@ import com.mchaves.sensors.device.management.common.IdGenerator;
 import com.mchaves.sensors.device.management.domain.model.Sensor;
 import com.mchaves.sensors.device.management.domain.model.SensorId;
 import com.mchaves.sensors.device.management.domain.repository.SensorRepository;
+import io.hypersistence.tsid.TSID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/api/sensors")
@@ -16,6 +18,13 @@ import org.springframework.web.bind.annotation.*;
 public class SensorController {
 
     private final SensorRepository sensorRepository;
+
+    @GetMapping("{sensorId}")
+    public SensorOutput getOne(@PathVariable("sensorId") TSID sensorId){
+        Sensor sensor = sensorRepository.findById(new SensorId(sensorId))
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        return convertToModel(sensor);
+    }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
@@ -31,6 +40,10 @@ public class SensorController {
                 .build();
 
         sensor = sensorRepository.saveAndFlush(sensor);
+        return convertToModel(sensor);
+    }
+
+    private SensorOutput convertToModel(Sensor sensor) {
         return SensorOutput.builder()
                 .id(sensor.getId().getValue())
                 .name(sensor.getName())
