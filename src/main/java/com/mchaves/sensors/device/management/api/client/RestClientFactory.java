c51f02e23e38ebfd8c00d839b93049798f5ed307
Mauricio Chaves <mauricio.chaves.jr@gmail.com>
2025-10-08 22:07:14 -0300

    develop: Simplificando a implementação com Factory Pattern

---
+++ src/main/java/com/mchaves/sensors/device/management/api/client/RestClientFactory.java
@@ -0,0 +1,37 @@
+package com.mchaves.sensors.device.management.api.client;
+
+import java.time.Duration;
+
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClient;
+
+import com.mchaves.sensors.device.management.api.client.exception.SensorMonitoringClientBadGatewayException;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class RestClientFactory {
+
+    private final RestClient.Builder builder;
+
+    public RestClient temperatureMonitoringRestClient() {
+        return builder.baseUrl("http://localhost:8082")
+                .requestFactory(generateClientHttpRequestFactory())
+                .defaultStatusHandler(HttpStatusCode::isError, (request, response) -> {
+                    throw new SensorMonitoringClientBadGatewayException();
+                })
+
+                .build();
+    }
+
+    private ClientHttpRequestFactory generateClientHttpRequestFactory() {
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        factory.setConnectTimeout(Duration.ofSeconds(5));
+        factory.setReadTimeout(Duration.ofSeconds(3));
+        return factory;
+    }
+}
--- src/main/java/com/mchaves/sensors/device/management/api/client/impl/SensorMonitoringClientImpl.java
+++ src/main/java/com/mchaves/sensors/device/management/api/client/impl/SensorMonitoringClientImpl.java
@@ -1,38 +1,20 @@
 package com.mchaves.sensors.device.management.api.client.impl;
 
-import com.mchaves.sensors.device.management.api.client.SensorMonitoringClient;
-import com.mchaves.sensors.device.management.api.client.exception.SensorMonitoringClientBadGatewayException;
-
-import io.hypersistence.tsid.TSID;
-
-import java.time.Duration;
-
-import org.springframework.http.HttpStatusCode;
-import org.springframework.http.client.ClientHttpRequestFactory;
-import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
+
+import com.mchaves.sensors.device.management.api.client.RestClientFactory;
+import com.mchaves.sensors.device.management.api.client.SensorMonitoringClient;
+
+import io.hypersistence.tsid.TSID;
 
 @Component
 public class SensorMonitoringClientImpl implements SensorMonitoringClient {
 
     private final RestClient restClient;
 
-    public SensorMonitoringClientImpl(RestClient.Builder builder) {
-        this.restClient = builder.baseUrl("http://localhost:8082")
-                .requestFactory(generateClientHttpRequestFactory())
-                .defaultStatusHandler(HttpStatusCode::isError, (request, response) -> {
-                    throw new SensorMonitoringClientBadGatewayException();
-                })
-
-                .build();
-    }
-
-    private ClientHttpRequestFactory generateClientHttpRequestFactory() {
-        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
-        factory.setConnectTimeout(Duration.ofSeconds(5));
-        factory.setReadTimeout(Duration.ofSeconds(3));
-        return factory; 
+    public SensorMonitoringClientImpl(RestClientFactory restClientFactory) {
+        this.restClient = restClientFactory.temperatureMonitoringRestClient();
     }
 
     @Override
